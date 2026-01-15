c93c6d0df4843394654c8a800f4e229b5a6881d1
viscode76 <justfortest9001@gmail.com>
2026-01-15 21:21:17 +0530

    vercel fix 3

--- server/routes/gigsRoutes.js
+++ server/routes/gigsRoutes.js
@@ -6,8 +6,8 @@ const { protected } = require("../middleware/protected");
 const { createGigs, getOpenGigs, getGigById, getAssignedGigs, reopenGig, deleteGig } = require("../controllers/gigsController");
 
 // mount router
-gigsRouter.post("/gigs", protected, createGigs);
 gigsRouter.get("/gigs", getOpenGigs);
+gigsRouter.post("/gigs", protected, createGigs);
 gigsRouter.get("/gigs/assigned", protected, getAssignedGigs);
 gigsRouter.get("/gigs/:gigId", getGigById);
 gigsRouter.put("/gigs/reopen/:gigId", protected, reopenGig);
